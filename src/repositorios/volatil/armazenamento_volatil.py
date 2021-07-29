f9c07f70494988579a0179ca452b965f1ea0111d
Renan Reschke <renanreschke@hotmail.com>
2021-07-29 19:39:08 -0300

    copia

---
+++ src/repositorios/volatil/armazenamento_volatil.py
@@ -0,0 +1,29 @@
+from devmaua.src.models.ra import RA
+from devmaua.src.models.usuario import Usuario
+
+from src.interfaces.interface_gerenciamento_usuarios import IArmazenamento
+
+
+class ArmazenamentoUsuarioVolatil(IArmazenamento):
+    armazem: list[Usuario]
+
+    def __init__(self):
+        self.armazem = []
+
+
+    def usuario_existe(self, outro_usuario: Usuario):
+        for u in self.armazem:
+            if u.nome == outro_usuario.nome and u.nascimento == outro_usuario.nascimento:
+                return True
+        return False
+
+    def cadastrar_usuario(self, usuario: Usuario):
+        self.armazem.append(usuario)
+
+
+    def get_usuario(self, ra: RA):
+        pass
+
+
+    def logar_usuario(self, login: str, senha: str):
+        pass
