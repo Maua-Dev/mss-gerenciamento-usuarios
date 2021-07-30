ce1daf26f1b653f50f70f97694b76c80059e6057
Renan Reschke <renanreschke@hotmail.com>
2021-07-30 19:08:35 -0300

    testes remover email

--- tests/usecases/test_remover_email.py
+++ tests/usecases/test_remover_email.py
@@ -19,6 +19,8 @@ from src.usecases.uc_remover_email import UCRemoverEmail
 
 from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroEmailInvalido
 from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroUsuarioInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroManipulacaoEmailFaculdade
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroDeletarEmailUnico
 
 class TestRemoverEmail:
     
@@ -70,3 +72,65 @@ class TestRemoverEmail:
         
         assert email not in repositorio.armazem[0].contato.emails
         assert len(repositorio.armazem[0].contato.emails) == 1
+        
+    def teste_erro_usuario_invalido(self):
+        repositorio = ArmazenamentoUsuarioVolatil()
+        removerEmail = UCRemoverEmail(repositorio)
+        
+        usuario = self.mockUsuario()
+        email = self.mockEmail()
+        
+        with pytest.raises(ErroUsuarioInvalido):
+            removerEmail.removerEmail(usuario, email)
+            
+    def test_erro_email_invalido(self):
+        repositorio = self.mockRepositorio()
+        removerEmail = UCRemoverEmail(repositorio)
+        
+        usuario = self.mockUsuario()
+        email = Email(email='teste@teste1.com',
+                      tipo=TipoEmail.PRIVADO,
+                      prioridade=1)
+                
+        with pytest.raises(ErroEmailInvalido):
+            removerEmail.removerEmail(usuario, email)
+            
+    def test_erro_erro_manipular_email_faculdade(self):
+        repositorio = self.mockRepositorio()
+        removerEmail = UCRemoverEmail(repositorio)
+        
+        usuario = self.mockUsuario()
+        email = Email(email='email@principal.com',
+                      tipo=TipoEmail.UNIVERSITARIO,
+                      prioridade=1)
+                
+        with pytest.raises(ErroManipulacaoEmailFaculdade):
+            removerEmail.removerEmail(usuario, email)
+            
+    def test_erro_deletar_email_unico(self):
+        repositorio = self.mockRepositorio()
+        removerEmail = UCRemoverEmail(repositorio)
+        
+        email = Email(email='teste@teste.com',
+                      tipo=TipoEmail.PRIVADO,
+                      prioridade=1)
+        
+        end = Endereco(logradouro='rua de tal',
+                       numero=20,
+                       cep='00000-000',
+                       tipo=TipoEndereco.RESIDENCIAL)
+        tel = Telefone(tipo=TipoTelefone.PRIVADO,
+                       numero='99999-9999',
+                       ddd=11,
+                       prioridade=3)
+        contato = Contato(emails=[email],
+                          telefones=[tel],
+                          enderecos=[end])
+
+        usuario = Usuario(nome='jorge do teste',
+                           contato=contato,
+                           nascimento= datetime.date(1999, 2, 23),
+                           roles=[Roles.ALUNO])
+        
+        with pytest.raises(ErroDeletarEmailUnico):
+            removerEmail.removerEmail(usuario, email)
