0a4b5b6b15aab9f4b5f24a0d7edf0d523e940963
Renan Reschke <renanreschke@hotmail.com>
2021-07-30 20:27:23 -0300

    UC para editar email

---
+++ src/usecases/uc_editar_email.py
@@ -0,0 +1,32 @@
+from devmaua.src.models.email import Email
+from devmaua.src.models.usuario import Usuario
+
+from devmaua.src.enum.tipo_email import TipoEmail
+
+from src.interfaces.interface_alteracao_infos_cadastro import IAlteracaoInfosCadastro
+
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroEmailInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroUsuarioInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroManipulacaoEmailFaculdade
+
+from typing import Optional
+
+
+class UCEditarEmail():
+    
+    alteracaoInfosCadastro: IAlteracaoInfosCadastro
+    
+    def __init__(self, alteracaoInfosCadastro: IAlteracaoInfosCadastro):
+        self.alteracaoInfosCadastro = alteracaoInfosCadastro
+        
+    def editarEmail(self, usuario: Usuario, email: Email, email_novo: Optional[str], tipo: Optional[TipoEmail], prioridade: Optional[int]):
+        if not(self.alteracaoInfosCadastro.usuarioExiste(usuario)):
+            raise ErroUsuarioInvalido
+        
+        if email == None or email not in usuario.contato.emails:
+            raise ErroEmailInvalido
+        
+        if email.tipo == TipoEmail.UNIVERSITARIO:
+            raise ErroManipulacaoEmailFaculdade
+        
+        self.alteracaoInfosCadastro.editarEmail(self, usuario, email, email_novo, tipo, prioridade)
