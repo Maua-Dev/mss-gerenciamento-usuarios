772f4b6e5b4ec006186944bd84420b3011ddc3ed
Renan Reschke <renanreschke@hotmail.com>
2021-07-30 19:00:20 -0300

    erros usecase remover email

--- src/usecases/erros/erros_uc_alteracao_info_cadastro.py
+++ src/usecases/erros/erros_uc_alteracao_info_cadastro.py
@@ -5,3 +5,11 @@ class ErroUsuarioInvalido(Exception):
 class ErroEmailInvalido(Exception):
     def __init__(self):
         super().__init__("Email invalido!")
+        
+class ErroManipulacaoEmailFaculdade(Exception):
+    def __init__(self):
+        super().__init__("Email da faculdade nao pode ser modificado")
+        
+class ErroDeletarEmailUnico(Exception):
+    def __init__(self):
+        super().__init__("Nao é possivel deletar o email pois ele é o único email cadastrado para o usuario")
