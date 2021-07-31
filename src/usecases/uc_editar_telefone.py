cc999c16b3806f6ac7c57a012976528c3116669c
Renan Reschke <renanreschke@hotmail.com>
2021-07-31 14:44:21 -0300

    UC editar telefone

---
+++ src/usecases/uc_editar_telefone.py
@@ -0,0 +1,28 @@
+from devmaua.src.models.telefone import Telefone
+from devmaua.src.models.usuario import Usuario
+
+from devmaua.src.enum.tipo_telefone import TipoTelefone
+
+from src.interfaces.interface_alteracao_infos_cadastro import IAlteracaoInfosCadastro
+
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroTelefoneInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroUsuarioInvalido
+
+from typing import Optional
+
+
+class UCEditarTelefone():
+    
+    alteracaoInfosCadastro: IAlteracaoInfosCadastro
+    
+    def __init__(self, alteracaoInfosCadastro: IAlteracaoInfosCadastro):
+        self.alteracaoInfosCadastro = alteracaoInfosCadastro
+        
+    def editarTelefone(self, usuario: Usuario, telefone: Telefone, tipo: Optional[TipoTelefone], ddd: Optional[int], numero: Optional[str]):
+        if not(self.alteracaoInfosCadastro.usuarioExiste(usuario)):
+            raise ErroUsuarioInvalido
+        
+        if telefone == None or telefone not in usuario.contato.telefones:
+            raise ErroTelefoneInvalido
+        
+        self.alteracaoInfosCadastro.editarTelefone(usuario, telefone, tipo, ddd, numero)
