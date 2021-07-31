7e29e246331332e5d4b359bf69608704ab99d7f1
Renan Reschke <renanreschke@hotmail.com>
2021-07-31 15:48:44 -0300

    UC remover endereco

---
+++ src/usecases/uc_remover_endereco.py
@@ -0,0 +1,28 @@
+from devmaua.src.models.endereco import Endereco
+from devmaua.src.models.usuario import Usuario
+
+from src.interfaces.interface_alteracao_infos_cadastro import IAlteracaoInfosCadastro
+
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroUsuarioInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroEnderecoInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroDeletarEnderecoUnico
+
+
+class UCRemoverEndereco():
+    
+    alteracaoInfosCadastro: IAlteracaoInfosCadastro
+    
+    def __init__(self, alteracaoInfosCadastro: IAlteracaoInfosCadastro):
+        self.alteracaoInfosCadastro = alteracaoInfosCadastro
+        
+    def removerEndereco(self, usuario: Usuario, endereco: Endereco):
+        if not(self.alteracaoInfosCadastro.usuarioExiste(usuario)):
+            raise ErroUsuarioInvalido
+        
+        if endereco == None or endereco not in usuario.contato.telefones:
+            raise ErroEnderecoInvalido
+        
+        if self.alteracaoInfosCadastro.quantidadeEnderecos(usuario) == 1:
+            raise ErroDeletarEnderecoUnico
+        
+        self.alteracaoInfosCadastro.removerEndereco(usuario, endereco)
