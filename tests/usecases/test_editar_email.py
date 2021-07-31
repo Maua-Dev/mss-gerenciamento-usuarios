c467042a641f8d770ad638029198a5296f04040d
Renan Reschke <renanreschke@hotmail.com>
2021-07-30 21:26:00 -0300

    teste unitario edição de email

---
+++ tests/usecases/test_editar_email.py
@@ -0,0 +1,128 @@
+import pytest
+import datetime
+
+from devmaua.src.models.contato import Contato
+from devmaua.src.models.email import Email
+from devmaua.src.models.endereco import Endereco
+from devmaua.src.models.telefone import Telefone
+from devmaua.src.models.usuario import Usuario
+
+from devmaua.src.enum.roles import Roles
+from devmaua.src.enum.tipo_email import TipoEmail
+from devmaua.src.enum.tipo_endereco import TipoEndereco
+from devmaua.src.enum.tipo_telefone import TipoTelefone
+
+from src.repositorios.volatil.armazenamento_volatil import ArmazenamentoUsuarioVolatil
+from src.usecases.cadastrar_usuario import CadastradorUsuario
+
+from src.usecases.uc_editar_email import UCEditarEmail
+
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroEmailInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroUsuarioInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroManipulacaoEmailFaculdade
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroDeletarEmailUnico
+
+
+class TestEditarEmail:
+    def mockUsuario(self) -> Usuario:
+        email1 = Email(email='email@principal.com',
+                      tipo=TipoEmail.UNIVERSITARIO,
+                      prioridade=1)
+        email2 = Email(email='teste@teste.com',
+                      tipo=TipoEmail.PRIVADO,
+                      prioridade=1)
+        
+        end = Endereco(logradouro='rua de tal',
+                       numero=20,
+                       cep='00000-000',
+                       tipo=TipoEndereco.RESIDENCIAL)
+        tel = Telefone(tipo=TipoTelefone.PRIVADO,
+                       numero='99999-9999',
+                       ddd=11,
+                       prioridade=3)
+        contato = Contato(emails=[email1, email2],
+                          telefones=[tel],
+                          enderecos=[end])
+
+        return Usuario(nome='jorge do teste',
+                           contato=contato,
+                           nascimento= datetime.date(1999, 2, 23),
+                           roles=[Roles.ALUNO])
+        
+    def mockEmail(self) -> Email:
+        return Email(email='teste@teste.com',
+                      tipo=TipoEmail.PRIVADO,
+                      prioridade=1)
+        
+    def mockRepositorio(self) -> ArmazenamentoUsuarioVolatil:
+        repositorio = ArmazenamentoUsuarioVolatil()
+        cadastrador = CadastradorUsuario(repositorio)
+        usuario = self.mockUsuario()
+        cadastrador.cadastrar(usuario)
+        return repositorio
+    
+    def test_editar_email(self):
+        repositorio = self.mockRepositorio()
+        editorEmail = UCEditarEmail(repositorio)
+        
+        usuario = self.mockUsuario()
+        email = self.mockEmail()
+        
+        email_novo = 'email@alterado.com'
+        tipo = TipoEmail.PRIVADO
+        prioridade = 2
+        
+        email_novo_instanciado = Email(email = email_novo,
+                                       tipo = tipo,
+                                       prioridade = prioridade)
+        
+        editorEmail.editarEmail(usuario, email, email_novo, tipo, prioridade)
+        
+        assert email_novo_instanciado in repositorio.getUsuarioPorNomeENascimento('Jorge Do Teste', datetime.date(1999, 2, 23)).contato.emails
+        assert email not in repositorio.getUsuarioPorNomeENascimento('Jorge Do Teste', datetime.date(1999, 2, 23)).contato.emails
+        
+    def test_erro_usuario_invalido(self):
+        repositorio = ArmazenamentoUsuarioVolatil()
+        editorEmail = UCEditarEmail(repositorio)
+        usuario = self.mockUsuario()
+        email = self.mockEmail()
+        
+        email_novo = 'email@alterado.com'
+        tipo = None
+        prioridade = 2
+        
+        with pytest.raises(ErroUsuarioInvalido):
+            editorEmail.editarEmail(usuario, email, email_novo, tipo, prioridade)
+            
+    def test_erro_email_invalido(self):
+        repositorio = self.mockRepositorio()
+        editorEmail = UCEditarEmail(repositorio)
+        
+        usuario = self.mockUsuario()
+        email = Email(email = 'nadave@teste.com',
+                      tipo = TipoEmail.PRIVADO,
+                      prioridade = 3)
+        
+        email_novo = 'email@alterado.com'
+        tipo = TipoEmail.PRIVADO
+        prioridade = 2
+        
+        with pytest.raises(ErroEmailInvalido):
+            editorEmail.editarEmail(usuario, email, email_novo, tipo, prioridade)
+        
+    def test_erro_editar_email_universitario(self):
+        repositorio = self.mockRepositorio()
+        editorEmail = UCEditarEmail(repositorio)
+        
+        usuario = self.mockUsuario()
+        email = Email(email='email@principal.com',
+                      tipo=TipoEmail.UNIVERSITARIO,
+                      prioridade=1)
+        
+        email_novo = 'email@alterado.com'
+        tipo = TipoEmail.PRIVADO
+        prioridade = 2
+        
+        with pytest.raises(ErroManipulacaoEmailFaculdade):
+            editorEmail.editarEmail(usuario, email, email_novo, tipo, prioridade)
+        
