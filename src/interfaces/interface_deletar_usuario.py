d228fd26798c6603005aea83c923d3c20107bbeb
Renan Reschke <renanreschke@hotmail.com>
2021-08-04 20:38:51 -0300

    Interface para deletar usuario

---
+++ src/interfaces/interface_deletar_usuario.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from devmaua.src.models.ra import RA
+
+
+class IDeletarUsuario(ABC):
+    """" Interface para deletar um usuario cadastrado no sistema """
+    
+    @abstractmethod
+    def removerAlunoPorRA(self, ra: RA):
+        """ Remove um usuario com a role 'Aluno' da base de usuarios """
+        pass
+    
