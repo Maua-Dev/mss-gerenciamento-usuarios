eda73e55ab34851b3e5f65929a63f14c2a94a94f
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-08-05 15:14:15 -0300

    controlador usando metodo de criar usuario por dict

--- src/controladores/control_cadastrar_usuario.py
+++ src/controladores/control_cadastrar_usuario.py
@@ -3,49 +3,27 @@ from devmaua.src.models.email import Email
 from devmaua.src.models.endereco import Endereco
 from devmaua.src.models.telefone import Telefone
 from devmaua.src.models.usuario import Usuario
+from devmaua.src.models.erros.erro_usuario import ErroDadosUsuarioInvalidos
 from fastapi import Request, Response
 from pydantic import ValidationError
 
 
 
-from src.usecases.erros.erros_usecase import ErroDadosUsuarioInvalidos
+
 from src.usecases.uc_cadastrar_usuario import UCCadastrarUsuario
 
 
 async def cadastrarUsuario(body, cadastrarUsuarioUC: UCCadastrarUsuario):
 
     try:
-        email = Email(email=body['contato']['emails'][0]['email'],
-                      tipo=body['contato']['emails'][0]['tipo'],
-                      prioridade=body['contato']['emails'][0]['prioridade'])
+        usuario = Usuario.criarUsuarioPorDict(body)
+        cadastrarUsuarioUC(usuario)
+        response = Response(content="Usuario criado com sucesso", status_code=200)
 
-        end = Endereco(logradouro=body['contato']['enderecos'][0]['logradouro'],
-                       numero=body['contato']['enderecos'][0]['numero'],
-                       cep=body['contato']['enderecos'][0]['cep'],
-                       tipo=body['contato']['enderecos'][0]['tipo'],
-                       complemento=body['contato']['enderecos'][0]['complemento'])
-        tel = Telefone(tipo=body['contato']['telefones'][0]['tipo'],
-                       numero=body['contato']['telefones'][0]['numero'],
-                       ddd=body['contato']['telefones'][0]['ddd'],
-                       prioridade=body['contato']['telefones'][0]['prioridade'])
-        contato = Contato(emails=[email],
-                          telefones=[tel],
-                          enderecos=[end])
+    except ErroDadosUsuarioInvalidos:
+        response = Response(content=ErroDadosUsuarioInvalidos(str(ErroDadosUsuarioInvalidos)), status_code=400)
 
-        usuario = Usuario(nome=body['nome'],
-                           contato=contato,
-                           nascimento=body['nascimento'],
-                           roles=body['roles'])
-
-    except ValidationError:
-        raise ErroDadosUsuarioInvalidos
-
-    except KeyError:
-        raise ErroDadosUsuarioInvalidos
-
-    cadastrarUsuarioUC(usuario)
-
-    return Response(content="Usuario criado com sucesso", status_code=200) #TODO Colocar excpectuib para use case diferente resp[onse
+    return response
 
 
 
