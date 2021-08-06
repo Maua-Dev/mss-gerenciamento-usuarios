502ef14b7803909b63ff93121ce53040eaad3b2b
Renan Reschke <renanreschke@hotmail.com>
2021-08-06 00:38:58 -0300

    Controller para adicionar email

---
+++ src/controladores/control_adicionar_email_fastapi.py
@@ -0,0 +1,39 @@
+from fastapi import Response
+
+from devmaua.src.models.usuario import Usuario
+from devmaua.src.models.email import Email
+from devmaua.src.models.erros.erro_usuario import ErroDadosUsuarioInvalidos
+from devmaua.src.models.erros.erro_email import ErroDadosEmailInvalidos
+
+from src.usecases.uc_adicionar_email import UCAdicionarEmail
+
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroEmailInvalido
+from src.usecases.erros.erros_uc_alteracao_info_cadastro import ErroUsuarioInvalido
+
+class ControllerHTTPAdicionarEmailFastAPI():
+    
+    def adicionarEmail(self, body: dict, adicionarEmailUC: UCAdicionarEmail):
+        
+        try:
+            usuario = Usuario.criarUsuarioPorDict(body['usuario'])
+            email = Email.criarEmailPorDict(body['email'])
+            
+            adicionarEmailUC.adicionarEmail(usuario, email)
+            response = Response(content="Email adicionado com sucesso", status_code=200)
+        
+        except ErroUsuarioInvalido:
+            response = Response(content=str(ErroUsuarioInvalido), status_code=400)
+            
+        except ErroEmailInvalido:
+            response = Response(content=str(ErroEmailInvalido), status_code=400)
+            
+        except ErroDadosUsuarioInvalidos:
+            response = Response(content=str(ErroDadosUsuarioInvalidos), status_code=400)
+            
+        except ErroDadosEmailInvalidos:
+            response = Response(content=str(ErroDadosEmailInvalidos), status_code=400)
+            
+        except KeyError:
+            response = Response(content=str(KeyError), status_code=400)
+            
+        return response
