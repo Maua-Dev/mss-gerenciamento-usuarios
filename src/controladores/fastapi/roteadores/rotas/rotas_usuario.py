1c2697a1b16f2719d79b2129f54e6c21c13408f1
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2021-11-01 23:01:09 -0300

    crrecao de erro antigo de rotas

--- src/controladores/fastapi/roteadores/rotas/rotas_usuario.py
+++ src/controladores/fastapi/roteadores/rotas/rotas_usuario.py
@@ -10,3 +10,15 @@ class RotasUsuario(APIRouter):
         @self.delete("")
         async def deletarUsuarioPorEmail(request: dict):
             return _ctrl.deletarUsuarioPorEmail(request)
+
+        @self.get("/{userId}")
+        async def getUsuarioPorUserId(userId: int):
+            return _ctrl.getUsuarioPorUserId(userId)
+
+        @self.get("/email/{email}")
+        async def getUsuarioPorEmail(email: str):
+            return _ctrl.getUsuarioPorEmail(email)
+
+        @self.get("/telefone/{ddd}/{numero}")
+        async def getUsuarioPorTelefone(ddd: int, numero: str):
+            return _ctrl.getUsuarioPorTelefone(ddd, numero)
