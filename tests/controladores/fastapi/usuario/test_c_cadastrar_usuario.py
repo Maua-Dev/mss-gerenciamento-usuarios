3a5880e22d4190f27c68bec5b0b95c13a2761fdd
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2021-11-02 00:02:14 -0300

    correcao de teste antigo

--- tests/controladores/fastapi/usuario/test_c_cadastrar_usuario.py
+++ tests/controladores/fastapi/usuario/test_c_cadastrar_usuario.py
@@ -1,58 +1,14 @@
 from src.controladores.fastapi.usuario.c_cadastrar_usuario import ControllerHTTPCadastrarUsuario
 from src.repositorios.mock.armazenamento_usuario_volatil import ArmazenamentoUsuarioVolatil
+import tests.mock_objetos as mo
 
 
-class TestControllerCadastrarUsuario():
+class TestControllerCadastrarUsuario:
     repoVolatil = ArmazenamentoUsuarioVolatil()
-    controllerCadastrarUsuario = ControllerHTTPCadastrarUsuario(repoVolatil)
-
-    def mockUsuario(self):
-        return {
-            "nome": "Jorge Do Teste",
-            "contato": {
-                "telefones": [
-                    {
-                        "tipo": 2,
-                        "numero": "99999-9999",
-                        "ddd": 11,
-                        "prioridade": 3
-                    }
-                ],
-                "emails": [
-                    {
-                        "email": "teste@teste.com",
-                        "tipo": 1,
-                        "prioridade": 1
-                    }
-                ],
-                "enderecos": [
-                    {
-                        "logradouro": "rua de tal",
-                        "numero": 20,
-                        "cep": "00000-000",
-                        "complemento": None,
-                        "tipo": 1
-                    }
-                ]
-            },
-            "nascimento": "1999-02-23",
-            "roles": [
-                9
-            ]
-        }
+    c = ControllerHTTPCadastrarUsuario(repoVolatil)
 
     def test_controller_cadastrar_usuario(self):
-        usuario = self.mockUsuario()
-        response = self.controllerCadastrarUsuario(body=usuario)
-        print(response)
-        print(dir(response))
+        usuario = mo.mockUsuario()
+        response = self.c(usuario)
 
         assert response.status_code == 200
-
-    def test_controller_cadastrar_sem_nome(self):
-        usuario = self.mockUsuario()
-        usuario['nome'] = None
-        response = self.controllerCadastrarUsuario(body=usuario)
-
-        assert response.status_code == 400
-
